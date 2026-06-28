27ff9b5360e40cb90bddd8f8349254335663b5c1
waliullahcoder <waliullahbiplob786@gmail.com>
2026-06-28 07:17:24 +0600

    added

--- resources/views/frontend/information/index.blade.php
+++ resources/views/frontend/information/index.blade.php
@@ -4,10 +4,6 @@
     <div class="hero-section" style="margin-bottom:20px;">
         <div class="container">
           <div class="section-card">
-                <div class="section-header mb-3">
-                    <h5 class="section-title text-center"> {{ $info->name }}</h5>
-                </div>  
-                
                     <p class="text-justify"> {!! $info->description !!}</p>
                     <br><br><br><br><br><br><br><br><br><br>
           </div>
